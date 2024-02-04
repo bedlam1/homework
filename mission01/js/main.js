a1968cb566a77df6084b2027bbc9a5593ae6a343
bedlam1 <90903692+bedlam1@users.noreply.github.com>
2024-02-04 14:00:34 +0900

    Update main.js

--- mission01/js/main.js
+++ mission01/js/main.js
@@ -23,7 +23,7 @@ let pw_valid = false;
 function emailReg(text){
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  return re.test(String(text).toLowerCase())
+  return re.test(String(text).toLowerCase());
 }
 
 function pwReg(text){
